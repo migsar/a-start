5b9bae2f6847d2771355fa558ff7242eaec1f7fa
Migsar Navarro <migsar.navarro@gmail.com>
2023-11-01 19:43:59 +0000

    chore: add vite root alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+	vite: {
+		resolve: {
+			alias: {
+				'~': path.resolve(__dirname, './src'),
+			},
+		},
+	},
 });
